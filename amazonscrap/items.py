812e92d7cadfe6367221d8f22020c3c21468c2ea
abhimangalms <abhimangalms@gmail.com>
2017-10-29 10:06:23 +0530

    Added variables in items.py

--- amazonscrap/items.py
+++ amazonscrap/items.py
@@ -9,6 +9,12 @@ from scrapy.item import Item, Field
 
 
 class AmazonscrapItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+	title = Field()
+	link = Field()
+	
+	
+	
+	
+	
+
+    
