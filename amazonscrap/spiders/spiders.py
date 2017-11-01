12f6eb6887c7d10548537f51390ba768273a75e8
abhimangalms <abhimangalms@gmail.com>
2017-11-01 08:50:47 +0530

    final commit

--- amazonscrap/spiders/spiders.py
+++ amazonscrap/spiders/spiders.py
@@ -5,22 +5,27 @@ import scrapy
 
 class MySpider(scrapy.Spider):
 	name = "adidas"
-	allowed_domains = ["amazon.in"]
+	#allowed_domains = ["amazon.in"]
   
-  #Use working product URL below
-  	start_urls = ["https://www.amazon.in/s/ref=nb_sb_ss_i_2_7?url=search-alias%3Dshoes&field-keywords=adidas+shoes&sprefix=adidas+%2Cundefined%2C353&crid=O7E4NE6341EK"]
+  	start_urls = ['https://www.amazon.in/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=adidas+shoes']
+	
  
  	def parse(self, response):
   		items = AmazonscrapItem()
-  		title = response.xpath('//h2[@class="s-access-title"]/span/text()').extract()	#extracting name
-  		sale_price = response.xpath('//span[contains(@id,"s-price")]/text()').extract()	#extracting price
-  		src = response.xpath('//img[@class="s-access-image cfMarker"]/@src').extract()	#extracting image
-		href = response.xpath('//a[@class="a-link-normal a-color-tertiary"]/text()').extract()	#extracting description
+		title = response.xpath('//div//a[@class ="a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal"]/@title').extract()	#extracting name
+  		sale_price = response.xpath('//span[@class ="a-size-base.a-color-price.s-price.a-text-bold"]/@text').extract()	#extracting price
+  		src = response.xpath('//img[@class ="s-access-image cfMarker"]/@src').extract()	#extracting image
+		href = response.xpath('//div//a[@class ="a-link-normal.s-access-detail-page.s-color-twister-title-link.a-text-normal"]/@href').extract()	#extracting description
   		
-  		items['name'] = ''.join(title).strip()
-  		items['price'] = ''.join(sale_price).strip()
-  		items['image'] = ''.join(src).strip()
-		items['description'] = ''.join(href).strip()
+  		items['name'] = ','.join(title).strip()
+  		items['price'] = ','.join(sale_price).strip()
+  		items['image'] = ','.join(src).strip()
+		items['description'] = ','.join(href).strip()
 		yield items
+		
+		
+	
+		
+
 
 			
