6ff8d3bdcd95f8136a4d56c109ad3fb6669ed609
abhimangalms <abhimangalms@gmail.com>
2017-11-15 23:02:12 +0530

    Some changes in spider.py

--- amazonscrap/spiders/spiders.py
+++ amazonscrap/spiders/spiders.py
@@ -10,17 +10,20 @@ class MySpider(scrapy.Spider):
 	
  	def parse(self, response):
   		items = AmazonscrapItem()
-		title = response.xpath('//div//a[@class ="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"]/@title').extract()	#extracting name
-  		sale_price = response.xpath('//span[@class ="a-size-base a-color-price s-price a-text-bold"]/@text').extract()	#extracting price
-  		src = response.xpath('//img[@class ="s-access-image cfMarker"]/@src').extract()	#extracting image
-		href = response.xpath('//div//a[@class ="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"]/@href').extract()	#extracting description
-  			
-		items['description'] = ','.join(href).strip()
-		items['image'] = ','.join(src).strip()
+		title = response.xpath('//a[@class ="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"]/@title').extract_first()	#extracting name
+  		prices = response.xpath('//*[@id="priceblock_ourprice"]/@text').extract_first()	#extracting price
+  		src = response.xpath('//img[@class ="s-access-image cfMarker"]/@src').extract_first()	#extracting image
+		href = response.xpath('//a[@class ="a-link-normal s-access-detail-page  s-color-twister-title-link a-text-normal"]/@href').extract_first()	#extracting description		
 		items['name'] = ','.join(title).strip()
-  		items['price'] = ','.join(sale_price).strip()
-		
+		items['price'] = ','.join(prices).strip()
+		items['image'] = ','.join(src).strip()
+		items['description'] = ','.join(href).strip()
+	
 		yield items
+		
+		
+		
+		
 		
 		
 	
